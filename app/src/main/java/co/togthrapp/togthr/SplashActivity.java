a01cf5f6bc78d3e12b02eb6d95f876afe213c7fd
Sabri K <sbrkrs@gmail.com>
2017-12-15 11:21:17 +0400

    splash screen checks for auth and takes you to main or login

--- app/src/main/java/co/togthrapp/togthr/SplashActivity.java
+++ app/src/main/java/co/togthrapp/togthr/SplashActivity.java
@@ -1,13 +1,35 @@
 package co.togthrapp.togthr;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
+
+    final int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                Intent intent;
+                if(firebaseUser == null){
+                    intent = new Intent(SplashActivity.this, Login.class);
+                }
+                else {
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                }
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
