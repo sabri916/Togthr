e1d0e1443a120e32beda274146af1ca0f6294342
Sabri K <sbrkrs@gmail.com>
2017-12-15 15:03:19 +0400

    profile icon and activity created

--- app/src/main/java/co/togthrapp/togthr/Fragments/TimelineFragment.java
+++ app/src/main/java/co/togthrapp/togthr/Fragments/TimelineFragment.java
@@ -1,6 +1,7 @@
 package co.togthrapp.togthr.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,9 +9,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
+import co.togthrapp.togthr.ProfileActivity;
 import co.togthrapp.togthr.R;
 import co.togthrapp.togthr.TimeLineAdapter;
 
@@ -30,8 +33,17 @@ public class TimelineFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View fragmentView = inflater.inflate(R.layout.fragment_timeline, container, false);
+
+        //Profile icon
+        ImageView profileImageView = (ImageView) fragmentView.findViewById(R.id.iv_icon_profile);
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+            }
+        });
+
         RecyclerView mRecyclerView = (RecyclerView) fragmentView.findViewById(R.id.rv_timeline);
         mRecyclerView.setLayoutManager(
                 new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,
